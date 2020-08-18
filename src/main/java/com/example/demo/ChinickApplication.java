e7b6c0d15283016935b0e73124e0805956f5953d
Jaites <jupadim.ac@gmail.com>
2020-08-18 18:18:21 +0100

    The first!

---
+++ src/main/java/com/example/demo/ChinickApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChinickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChinickApplication.class, args);
+	}
+
+}
